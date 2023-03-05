81ae847c22ea75bdfd6d03e21e923b250ea95539
Sachin Wanniarachchi <sachinwanniarachchi@gmail.com>
2023-03-05 08:53:41 -0800

    Created color guide plate

---
+++ MyFitness/Resources/ColorGuide.swift
@@ -0,0 +1,19 @@
+//
+//  ColorGuide.swift
+//  MyFitness
+//
+//  Created by Sachin on 3/5/23.
+//
+
+import UIKit
+
+final class ColorGuide {
+    
+    static let shared = ColorGuide()
+    
+    static var primary = UIColor(rgb: 0xEF511B)
+    static var secondary = UIColor(rgb: 0x6E6D7A)
+    static var light = UIColor(rgb: 0xFFFFFF)
+    static var dark = UIColor(rgb: 0x000000)
+    
+}
