b75d1d9b5a04922fd88490f495706462d7aa0165
Sachin Wanniarachchi <sachinwanniarachchi@gmail.com>
2023-05-23 10:46:52 -0700

    Add AuthManager file

---
+++ MyFitness/Managers/AuthManager.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  MyFitness
+//
+//  Created by Sachin on 5/18/23.
+//
+
+import Foundation
+
+final class AuthManager {
+    
+    static let shared = AuthManager()
+    
+    static var username = "sachin"
+    static var fullName = "Michael Bernardo"
+    static var gender = ""
+    static var height = "0.00"
+    static var weight = "0.00"
+    static var fitnessGoal = ""
+    static var difficultyLevel = 1
+}
