3462b13a7192eeff3a4e6ab7111ffc8b24aad1d5
Sachin Wanniarachchi <sachinwanniarachchi@gmail.com>
2023-05-24 08:19:01 -0700

    Add AuthManager

--- MyFitness/Managers/AuthManager.swift
+++ MyFitness/Managers/AuthManager.swift
@@ -11,11 +11,12 @@ final class AuthManager {
     
     static let shared = AuthManager()
     
+    static var isSignIn = false
     static var username = "sachin"
     static var fullName = "Michael Bernardo"
     static var gender = ""
     static var height = "0.00"
     static var weight = "0.00"
     static var fitnessGoal = ""
-    static var difficultyLevel = 1
+    static var difficultyLevel = 0
 }
