18092d39ea374fded1d3556fe82d45ec33e88b3b
Sachin Wanniarachchi <sachinwanniarachchi@gmail.com>
2023-05-27 09:14:55 -0700

    SearchExercise Model

---
+++ MyFitness/Models/SearchExercise.swift
@@ -0,0 +1,14 @@
+//
+//  SearchExercise.swift
+//  MyFitness
+//
+//  Created by Sachin on 5/11/23.
+//
+
+import Foundation
+
+// MARK: - Exercise Model
+struct SearchExerciseModel: Codable {
+    let id: String
+    let name: String
+}
